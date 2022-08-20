9413ae98324d0a9182d7f841316f861f533f7641
RenatusRS <freekill246@gmail.com>
2022-08-20 11:38:51 +0200

    Update CircularArray.java

--- src/CircularArray.java
+++ src/CircularArray.java
@@ -7,7 +7,7 @@ public class CircularArray<T> {
 	private int start = 0;
 	private int limit;
 
-	CircularArray(int limit) {
+	public CircularArray(int limit) {
 		this.limit = limit;
 	}
 
@@ -58,58 +58,6 @@ public class CircularArray<T> {
 		limit = newLimit;
 		arr = newArr;
 		start = 0;
-	}
-
-	public static void main(String args[]) {
-		CircularArray<Integer> c1 = new CircularArray<>(10);
-
-		for (int i = 0; i < 10; i++)
-			c1.add(i);
-
-		for (int i = 0; i < c1.size(); i++)
-			System.out.print(c1.get(i) + " ");
-		System.out.println();
-		System.out.println(c1.start);
-		System.out.println();
-
-		for (int i = 10; i < 15; i++)
-			c1.add(i);
-
-		for (int i = 0; i < c1.size(); i++)
-			System.out.print(c1.get(i) + " ");
-		System.out.println();
-		System.out.println(c1.start);
-		System.out.println();
-
-		c1.limit(7);
-
-		for (int i = 0; i < c1.size(); i++)
-			System.out.print(c1.get(i) + " ");
-		System.out.println();
-
-		c1.limit(12);
-
-		for (int i = 0; i < c1.size(); i++)
-			System.out.print(c1.get(i) + " ");
-		System.out.println();
-
-		for (int i = 15; i < 20; i++)
-			c1.add(i);
-
-		for (int i = 0; i < c1.size(); i++)
-			System.out.print(c1.get(i) + " ");
-		System.out.println();
-		System.out.println(c1.start);
-		System.out.println();
-
-		for (int i = 20; i < 25; i++)
-			c1.add(i);
-
-		for (int i = 0; i < c1.size(); i++)
-			System.out.print(c1.get(i) + " ");
-		System.out.println();
-		System.out.println(c1.start);
-		System.out.println();
 	}
 
 }
